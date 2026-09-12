4baa8962ed38eaddd8440806caa79e9edef2e24c
Maximiik <mrudalovskiy@gmail.com>
2026-09-13 00:47:14 +0300

    migration

---
+++ NewsAggregator.Infrastructure/Migrations/20260912143002_AddUniqueIndexOnArticleUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsAggregator.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexOnArticleUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Url",
+                table: "Articles",
+                column: "Url",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_Url",
+                table: "Articles");
+        }
+    }
+}
